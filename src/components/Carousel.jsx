3ba49fece71ca92f7f4ed71b80952999749e2b10
selamufoffano <selamufoffano778@gmail.com>
2026-03-03 16:39:04 +0100

    carosello funzionante

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,35 +1,45 @@
 import LoadingSkeleton from "./LoadingSkeleton";
-
-let carosello = document.getElementById("CarouselMove");
-const CarouselMoveFlet = ()=> {
-// ogni volta che viene premuto il bottone
-// il carosello deve essere mosso X
-console.log("Il botonne move left premuto");
-carosello.style.transition=-222;
-}
-
-const CarouselMoveRighet = ()=> {
-console.log("Il botonne move right premuto");
-
-}
-
-
+import { useRef } from "react";
 
 export const Carousel = ({}) => {
+  let carouselMove = useRef(null);
+
+  const CarouselMoveFlet = () => {
+    carouselMove.current.scrollBy({
+      left: -600,
+      behavior: "smooth",
+    });
+  };
+
+  const CarouselMoveRighet = () => {
+    carouselMove.current.scrollBy({
+      left: 600,
+      behavior: "smooth",
+    });
+  };
+
   return (
     <div className="">
       <div className="flex justify-end w-full h-7 gap-4">
-        <button onClick={CarouselMoveFlet} class="bg-[#222222] rounded-md border border-white/5 group cursor-pointer hover:bg-[#161616] transition-all px-1 text-white">
+        <button
+          onClick={CarouselMoveFlet}
+          class="bg-[#222222] rounded-md border border-white/5 group cursor-pointer hover:bg-[#161616] transition-all px-1 text-white"
+        >
           Left
         </button>
-        <button onClick={CarouselMoveRighet} class="bg-[#222222] rounded-md border border-white/5 group cursor-pointer hover:bg-[#161616] transition-all px-1 text-white">
+        <button
+          onClick={CarouselMoveRighet}
+          class="bg-[#222222] rounded-md border border-white/5 group cursor-pointer hover:bg-[#161616] transition-all px-1 text-white"
+        >
           Right
         </button>
       </div>
-      <div className="flex w-full h-100 overflow-x-auto snap-x snap-mandatory scroll-smooth">
-        {Array.from({ length: 10 }).map((_, i) => (
+      <div
+        ref={carouselMove}
+        className="flex w-full h-100 overflow-x-hidden snap-x snap-mandatory scroll-smooth"
+      >
+        {Array.from({ length: 20 }).map((_, i) => (
           <div
-            id="CarouselMove"
             key={i}
             className="min-w-[300px] h-full snap-center flex-shrink-0 p-4"
           >
